cd69ec7e78751387fc0785e0a15441cc627478ae
Carmine Faella <carminefaella5@gmail.com>
2023-03-27 14:11:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+
+export default{
+  components:{
+    TheHeader,
+    TheFooter
+  }
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style>
+</style>
+
