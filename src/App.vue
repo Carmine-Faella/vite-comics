72e9582d7aa5fcf978c1e9c0e843fcf8e89cbfd3
Carmine Faella <carminefaella5@gmail.com>
2023-03-27 15:31:21 +0200

    Implementazione Footer

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,97 @@ export default{
   components:{
     TheHeader,
     TheFooter
-  }
+  },
+  data(){
+    return{
+      choises:[
+        {
+          title:'DIGITAL COMICS',
+          image:'buy-comics-digital-comics.png'
+        },
+        {
+          title:'DC MERCHANDISE',
+          image:'buy-comics-merchandise.png'
+        },
+        {
+          title:'SUBSCRIPTION',
+          image:'buy-comics-subscriptions.png'
+        },
+        {
+          title:'COMIC SHOP LOCATOR',
+          image:'buy-comics-shop-locator.png'
+        },
+        {
+          title:'DC POWER VISA',
+          image:'buy-dc-power-visa.svg'
+        }
+      ]
+    }
+  },
+    methods:{
+      getImagePath: function(img){
+        return new URL(`./assets/${img}`, import.meta.url).href;
+      }
+    }
 }
 
 </script>
 
 <template>
+  <header>
+    <TheHeader></TheHeader>
+  </header>
+  <main>
+    <div>Content goes here</div>
+    <section class="cnt-choises">
+      <div class="item-icon">
+        <div v-for="item in choises" class="image-wrap">
+          <div class="img-wrapper"><img :src="getImagePath(item.image)" alt="Immmagine"></div>
+          <span><a href="#">{{item.title}}</a></span>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <TheFooter></TheFooter>
+  </footer>
   
 </template>
 
-<style>
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use './styles/partials/mixins.scss' as *;
+@use './styles/partials/variables.scss' as *;
+
+.item-icon{
+  display: flex;
+  justify-content: center;
+  padding: 4rem 0;
+  @include container;
+
+  .image-wrap{
+    display: flex;
+    align-items: center;
+
+      a{
+        color: $color-font;
+        font-size: 0.7rem;
+        padding: 0 1.5rem;
+      }
+
+      .img-wrapper{
+      width: 50px;
+      height: 50px;
+
+        img{
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+
+        }
+    }
+  }
+
+}
 </style>
 
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,96 @@
 <script>
 export default{
     data(){
-        return{}
+        return{
+            option: [
+                {
+                    title:'CHARACTERS',
+                    href:'#CHARACTERS'
+                },
+                {
+                    title:'COMICS',
+                    href:'#COMICS'
+                },
+                {
+                    title:'MOVIES',
+                    href:'#MOVIES'
+                },
+                {
+                    title:'TV',
+                    href:'#TV'
+                },
+                {
+                    title:'GAMES',
+                    href:'#GAMES'
+                },
+                {
+                    title:'COLLECTIBLES',
+                    href:'#COLLECTIBLES'
+                },
+                {
+                    title:'VIDEOS',
+                    href:'#VIDEOS'
+                },
+                {
+                    title:'FANS',
+                    href:'#FANS'
+                },
+                {
+                    title:'NEWS',
+                    href:'#NEWS'
+                },
+                {
+                    title:'SHOP',
+                    href:'#SHOP'
+                }
+            ],
+        }
     }
 }
 </script>
 
 <template>
-  
+  <nav>
+    <div class="logo"> 
+        <img src="../assets/dc-logo.png" alt="logo Dc">
+    </div>
+    <ul class="list-nav">
+        <li v-for="list in option"><a :href="list.href">{{list.title}}</a></li>
+    </ul>
+  </nav>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+nav{
+    height: 15vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    @include container;
+    
+
+    .list-nav{
+        display: flex;
+
+        li{
+            padding: 1rem;
+            font-size: 0.8rem;
+            list-style:none;
+
+            a{
+                color: $color-primary;;
+            }
+        }
+    }
+}
+
+img{
+    object-fit: contain;
+    height: 3rem;    
+    padding-left: 2rem;    
+}
 
 </style>
